d337e90ded8bde47d08ddf0afecce86bb898f61f
atelier-ritz <xutianqish@gmail.com>
2018-03-22 14:11:29 -0400

    added oscBetween()
    makes it easier to oscillate between 2 values in the subthread

--- mathfx.py
+++ mathfx.py
@@ -4,3 +4,16 @@ def cosd(a):
 
 def sind(a):
     return sin(radians(a))
+
+def oscBetween(currentTime,oscShape,frequency,lowerBound,upperBound):
+    ''' allows oscillation between lowerBound and upperBound '''
+    if oscShape == 'sin':
+        return 0.5 * (lowerBound+upperBound) + (upperBound-lowerBound)*0.5 * sin(2*pi*frequency*currentTime)
+    elif oscShape == 'saw':
+        return lowerBound + currentTime*frequency*(upperBound-lowerBound) % (upperBound-lowerBound)
+    elif oscShape == 'square':
+        return upperBound if int(frequency * 2 * t) % 2 == 0 else lowerBound
+    elif oscShape == 'triangle':
+        return lowerBound + abs((lowerBound-upperBound) + currentTime*frequency*2*(upperBound-lowerBound) % (2*(upperBound-lowerBound)))
+    else:
+        return 0
